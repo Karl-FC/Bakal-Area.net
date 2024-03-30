4d1fe97a6ce8b666e6598ed454c578773d28b5b8
KFCris <karlfcris5@gmail.com>
2024-03-30 18:47:52 +0800

    Merged Changes
    Merged Changes from V2 and V3.
     - Added Navbar
     - Added dark mode
     - Axial Compression
         - Flange and web compactness check
         - Connected to AISC 15 database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { FormsModule } from '@angular/forms'; // Add this line at the top
 
 @NgModule({
@@ -8,21 +7,6 @@ import { FormsModule } from '@angular/forms'; // Add this line at the top
   imports: [
     // ...
     FormsModule, // Add this line
-=======
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
->>>>>>> fa38da51d8686245a1bbb2f9dda78a0dbfbfb915
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/components/navbar/navbar.component.spec.ts
+++ src/app/shared/components/navbar/navbar.component.spec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
@@ -22,28 +21,3 @@ describe('NavbarComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-=======
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { NavbarComponent } from './navbar.component';
-
-describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(NavbarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
->>>>>>> fa38da51d8686245a1bbb2f9dda78a0dbfbfb915
